aae0327688b0b563f6244cd10410997c0945b6f0
Podologia-Martita <chelomiranda64@gmail.com>
2025-09-28 23:18:07 -0300

    Update ProfessionalsList.jsx

--- src/components/ProfessionalsList.jsx
+++ src/components/ProfessionalsList.jsx
@@ -1,1 +1,22 @@
+import { useEffect, useState } from 'react';
+import { fetchProfessionals } from '../fetchProfessionals'; // nota el ../ si fetchProfessionals.js está en src/
+
+function ProfessionalsList() {
+  const [professionals, setProfessionals] = useState([]);
+
+  useEffect(() => {
+    fetchProfessionals().then(data => setProfessionals(data));
+  }, []);
+
+  return (
+    <div>
+      {professionals.length === 0 && <p>No hay profesionales disponibles</p>}
+      {professionals.map(p => (
+        <div key={p.id}>{p.name}</div>
+      ))}
+    </div>
+  );
+}
+
+export default ProfessionalsList;
 
