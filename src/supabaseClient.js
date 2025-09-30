042920908be4cda670d69b0e72b90b54a6712701
Podologia-Martita <chelomiranda64@gmail.com>
2025-09-29 21:47:50 -0300

    Actualizar supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL=https://oeczqyusjsbvfchyofuo.supabase.co
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lY3pxeXVzanNidmZjaHlvZnVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwODAzNzgsImV4cCI6MjA3NDY1NjM3OH0.foUxwGTM9ICmI46KO3WxAxf4uuhJycFPyY2q4rHv52k
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
